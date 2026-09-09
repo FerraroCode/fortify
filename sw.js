bfaf97f44f8e4a9d951b20320a4832dedc009eec
FerraroCode <70236785+FerraroCode@users.noreply.github.com>
2026-09-09 11:08:53 -0400

    Cache Fortify+ premium feature bundle

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='fortify-v6';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./config.js','./cloud.js','./auth.js','./billing.js','./manifest.json'];
+const CACHE='fortify-v7';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./config.js','./cloud.js','./auth.js','./billing.js','./premium.js','./manifest.json'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener('fetch',e=>{
