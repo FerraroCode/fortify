1f3277d6622159dda6b1d9cf7590bbe2a4f34d59
FerraroCode <70236785+FerraroCode@users.noreply.github.com>
2026-09-09 10:52:24 -0400

    Trigger Fortify production deploy after Git connection

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='fortify-v5';
+const CACHE='fortify-v6';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./config.js','./cloud.js','./auth.js','./billing.js','./manifest.json'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
